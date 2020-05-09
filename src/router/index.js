51febdc21de9d156a94cfd0c2a8b5a96382da0bb
WOOYOUNG JEONG <hellojohnwoo@gmail.com>
2020-05-09 23:35:14 +0900

    0509 read, create +@ in board vuejs

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import Read from '@/components/Read'
 import Create from '@/components/Create'
 
